816f579eda8260c3b2baa41a7d306cbf8cf8de37
Mohamed Aly Sidibe <mouhamed.aly.sidibe96@gmail.com>
2020-05-14 13:39:52 +0000

    adding pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-native-web`,
-    /* ... */
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Expo-Gatsby-Starter`,
+        short_name: `ExpoGatsby`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#cccccc`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 };
